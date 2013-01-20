9141d9c2ce42e396f4c214521136b83ee8a1c9d7
Fedake <braveside@gmail.com>
2013-01-20 18:50:32 +0100

    Dodane Motherboard i Memory

--- Form1.cs
+++ Form1.cs
@@ -31,15 +31,6 @@ namespace Diag
                 }
             }
 
-            searcher.Query = new ObjectQuery("SELECT * FROM Win32_VideoController");
-            foreach (ManagementObject obj in searcher.Get())
-            {
-                foreach (PropertyData data in obj.Properties)
-                {
-                    if (data.Name == "Name") this.videoCardName.Text = data.Value.ToString();
-                }
-            }
-
             searcher.Query = new ObjectQuery("SELECT * FROM Win32_CacheMemory");
             foreach (ManagementObject obj in searcher.Get())
             {
@@ -66,6 +57,70 @@ namespace Diag
                     }
                 }
             }
+
+			searcher.Query = new ObjectQuery("SELECT * FROM Win32_BaseBoard");
+			foreach (ManagementObject obj in searcher.Get())
+			{
+				foreach (PropertyData data in obj.Properties)
+				{
+					if (data.Name == "Manufacturer") this.moboManuBox.Text = data.Value.ToString();
+					if (data.Name == "Product") this.moboModelBox.Text = data.Value.ToString();
+				}
+			}
+
+			searcher.Query = new ObjectQuery("SELECT * FROM Win32_BIOS");
+			foreach (ManagementObject obj in searcher.Get())
+			{
+				foreach (PropertyData data in obj.Properties)
+				{
+					if (data.Name == "Manufacturer") this.biosManuBox.Text = data.Value.ToString();
+					if (data.Name == "SMBIOSBIOSVersion") this.biosVerBox.Text = data.Value.ToString();
+				}
+			}
+
+			searcher.Query = new ObjectQuery("SELECT * FROM Win32_PhysicalMemory");
+
+			ulong totalSize = 0;
+			foreach (ManagementObject obj in searcher.Get())
+			{
+				foreach (PropertyData data in obj.Properties)
+				{
+					if (data.Name == "Capacity") totalSize += (ulong)data.Value;
+				}
+			}
+			totalSize = totalSize/1024/1024;
+			this.memorySizeBox.Text = totalSize.ToString() + " MB";
+
+
+            searcher.Query = new ObjectQuery("SELECT * FROM Win32_VideoController");
+			uint w = 0, h = 0, r = 0;
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                foreach (PropertyData data in obj.Properties)
+                {
+                    if (data.Name == "Name") this.videoCardName.Text = data.Value.ToString();
+
+					if (data.Name == "CurrentHorizontalResolution") w = (uint)data.Value;
+					if (data.Name == "CurrentVerticalResolution") h = (uint)data.Value;
+					if (data.Name == "CurrentRefreshRate") r = (uint)data.Value;
+                }
+            }
+			resolutionBox.Text = w.ToString() + " x " + h.ToString() + " @ " + r.ToString() + "Hz";
         }
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
     }
 }
